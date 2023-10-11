0f793a7e497f99d582f5892a07f86cf6b7aab409
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-10-11 20:59:23 +0800

    API Routes setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ChatMessageController;
+use App\Http\Controllers\ConversationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -16,6 +18,28 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/users', [UserController::class, 'getList']);
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::prefix('chat')->group(function() {
+        Route::prefix('message')->group(function() {
+            Route::get('/{id}', [ChatMessageController::class, 'get']);
+            Route::post('/send', [ChatMessageController::class, 'send']);
+        });
+
+        Route::prefix('conversation')->group(function() {
+            Route::get('/', [ConversationController::class, 'get']);
+            Route::post('/listen', [ConversationController::class, 'listen']);
+            Route::post('/new', [ConversationController::class, 'createConversation']);
+        });
+    });
+});
+
+Route::get('/test', function(Request $req) : Illuminate\Http\JsonResponse {
+    dd(explode(' ', ''));
+    return response()->json([
+        'jakolera ka' => true
+    ]);
 });
